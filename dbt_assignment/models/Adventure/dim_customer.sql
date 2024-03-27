1b935f039ba52faba2acbfd350beafaf5d038ba7
chaurasia-rahul <rahul.chaurasia@bittwovyte.com>
2024-03-27 16:17:38 +0530

    commit24

--- dbt_assignment/models/Adventure/dim_customer.sql
+++ dbt_assignment/models/Adventure/dim_customer.sql
@@ -1,32 +1,36 @@
-{{ config(materialized='incremental',unique_key='customer_id') }}   -- modify script as per 
+{{ config(materialized='incremental',
+   unique_key='customer_id',
+    pre_hook='{% if is_incremental() %} DELETE FROM {{ this }} WHERE MODIFIED_DATE <> CURRENT_DATE {% endif %}') }}  
 
-with CUST AS(
-select CUSTOMER_ID,
-	   PERSON_ID,
-	   TERRITORY_ID as CUST_TERRITORY_ID,
-	   ACCOUNT_NUMBER,
-	   ROWGUID as CUST_ROWGUID,
-	   MODIFIED_DATE as CUST_MODIFIED_DATE
+with cust AS(
+select *
 from {{ source('stg', 'customer') }}
 ),
-TERRITORY as (
-select TERRITORY_ID as TERR_TERRITORY_ID,
-	   NAME,
-	   COUNTRY_REGION_CODE,
-	   "group",
-	   SALES_YTD,
-	   SALES_LAST_YEAR,
-	   COST_YTD,
-	   COST_LAST_YEAR,
-	   ROWGUID as TERRITORY_ROWGUID,
-	   MODIFIED_DATE as TERR_MODIFIED_DATE
-from{{ source('stg', 'salesterritory') }}
+Territory as (
+select *
+from {{ source('stg', 'salesterritory') }} 
+),
+final_cust as(
+select customer_id::int,
+store_id::int,
+account_number::varchar(50),
+c.modified_date::timestamp,
+person_id::int,
+c.territory_id::int,
+name::varchar(50),
+country_region_code::varchar(50),
+"group"::varchar(50),
+sales_ytd::numeric(38, 9),
+sales_last_year::numeric(38, 9),
+cost_ytd::numeric(38, 9),
+cost_last_year::numeric(38, 9)
+from cust c
+left join territory b on c.TERRITORY_ID = b.TERRITORY_ID 
 )
 select *
-from CUST 
-left join TERRITORY on CUST.CUST_TERRITORY_ID = TERRITORY.TERR_TERRITORY_ID
+from final_cust
+where 1=1
 
-
-  {% if is_incremental() %}
-  and cust.CUST_MODIFIED_DATE::timestamp > (select max(CUST_MODIFIED_DATE) from {{this}})
-  {% endif %}
+ -- {% if is_incremental() %}
+--  and MODIFIED_DATE::timestamp > (select max(MODIFIED_DATE) from {{this}})
+ -- {% endif %}
--- dbt_assignment/models/Adventure/dim_product_inc.sql
+++ dbt_assignment/models/Adventure/dim_product_inc.sql
@@ -1,78 +1,65 @@
-{{config(materialized='incremental',alias= 'dim_product_inc',unique_key='product_id')}}   --pre-hook -- add cte table for flag column
+{{
+  config(materialized='incremental', alias= 'dim_product_inc', unique_key='product_id',
+  pre_hook='{% if is_incremental() %} DELETE FROM {{ this }} WHERE MODIFIED_DATE <> CURRENT_DATE {% endif %}'
+  )
+}}
 
-with product AS (
-select product_id
-,Product_name
-,product_number
-,MAKE_FLAG
-,finished_goods_flag
-,safety_stock_level
-,reorder_point
-,standard_cost
-,list_price
-,days_to_manufacture
-,sell_start_date
-,modified_date
-,color
-,class
-,weight_unit_measure_code
-,weight
-,size
-,size_unit_measure_code
-,product_line
-,style
-,product_subcategory_id
-,product_model_id
-,sell_end_date
-, CASE WHEN make_flag = '1' and size = null then 1 ELSE 0 end as is_make_no_size
+with product_flag as (
+select *
+, CASE WHEN make_flag = '1' and size is null then 1 ELSE 0 end as is_make_no_size
 , CASE WHEN finished_goods_flag = '1' and weight > 25 then 1 ELSE 0 end as is_finished_and_over_25
-from {{ ref('product_mod') }}  -- actual table
+from {{ source('stg','product') }}  
+),
+product AS (
+select *,
+case when is_make_no_size = 1 or is_finished_and_over_25 = 1 then 1 else 0 end as is_rel
+from product_flag  
 ),
 Productcategory as (
-select product_category_id,
-       name as Productcat_name
+select *
 from {{ source('stg', 'productcategory') }} 
 ),
 subcategory AS (
-select product_subcategory_id,
-       product_category_id,
-       name as Subcategory_name
+select *
 from {{ source('stg', 'productsubcategory') }}
-)
-select product_id
-,Product_name
-,product_number
-,MAKE_FLAG
-,is_make_no_size
-,finished_goods_flag
-,is_finished_and_over_25
-,case when is_make_no_size = 1 or is_finished_and_over_25 = 1 then 1 else 0 end as is_rel
-,safety_stock_level
-,reorder_point
-,standard_cost
-,list_price
-,days_to_manufacture
-,sell_start_date
-,modified_date
-,color
-,"class"
-,weight_unit_measure_code
-,weight
-,size
-,size_unit_measure_code
-,product_line
-,style
-,product_model_id
-,sell_end_date
-,b.product_category_id
-,Productcat_name
-,c.product_subcategory_id
-,Subcategory_name 
+),
+final_product AS (
+select product_id::int
+,a.name::varchar(30) as Product_name 
+,product_number::varchar(30)
+,MAKE_FLAG::bool
+,is_make_no_size::int
+,finished_goods_flag::bool
+,is_finished_and_over_25::int
+,is_rel::int
+,safety_stock_level::int
+,reorder_point::int
+,standard_cost::numeric(38, 9)
+,list_price::numeric(38, 9)
+,days_to_manufacture:: int
+,sell_start_date::timestamp
+,a.modified_date::timestamp
+,color::varchar(30)
+,"class"::varchar(30)
+,weight_unit_measure_code::varchar(30)
+,weight::numeric(38, 9)
+,size::varchar(30)
+,size_unit_measure_code::varchar(30)
+,product_line::varchar(30)
+,style::varchar(30)
+,product_model_id::int
+,sell_end_date::timestamp
+,b.product_category_id::int
+,b.name::varchar(30) as Category_name
+,c.product_subcategory_id::int
+,c.name::varchar(30) as Subcategory_name 
 from product a
 inner join Productcategory b on a.product_id = b.product_category_id
 inner join subcategory c on b.product_category_id = c.product_category_id
+)
+select *
+from final_product
 
-
-  {% if is_incremental() %}
-  and MODIFIED_DATE::timestamp > (select max(MODIFIED_DATE) from {{this}})
-  {% endif %}
+--  {% if is_incremental() %}
+--    WHERE modified_date::timestamp > (select max(MODIFIED_DATE) from {{this}})
+--  {% endif %}
