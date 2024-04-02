e5c5c599fa7aefef4816d39b6cc83c31108e3db9
chaurasia-rahul <rahul.chaurasia@bittwovyte.com>
2024-04-02 19:54:58 +0530

    commit02040754

--- dbt_assignment/macros/yealry_split.sql
+++ dbt_assignment/macros/yealry_split.sql
@@ -3,7 +3,7 @@
 {% macro yearly_split(year) %}
 
 {{ config(materialized='table', alias='sales_order_item_'~year) }}
-SELECT s.*, s2.sub_total, s2.total_due, EXTRACT(YEAR FROM Order_date) AS "Year"
+SELECT s.*, EXTRACT(YEAR FROM Order_date) AS "Year"
 FROM STG.salesorderdetail s 
 INNER JOIN STG.salesorderheader s2 
 ON s.sales_order_id = s2.sales_order_id 
--- dbt_assignment/models/Adventure/fact_sales.sql
+++ dbt_assignment/models/Adventure/fact_sales.sql
@@ -1,15 +1,41 @@
-{%- set years = ['2011', '2012', '2013', '2014'] -%}
+{{ config(materialized='incremental', unique_key=['sales_order_id','sales_order_detail_id'], alias='fact_sales_order') }}
+
+WITH cte_sales_order AS (
+ 
+{%- set years = run_query("SELECT DISTINCT EXTRACT(YEAR FROM order_date)::INT AS year FROM stg.salesorderheader ") %}
 
 {%- for year in years -%}
-    {{ config(materialized='incremental', unique_key=['sales_order_id', 'sales_order_detail_id'], alias='fact_sales_order') }}
-
+      
         select *
-        from dwh.sales_order_item_{{ year }} 
+        from dwh.sales_order_item_{{ year.year }}
         where 1=1
-         {% if is_incremental() %}
-           and MODIFIED_DATE::timestamp > (select max(MODIFIED_DATE) from {{ this }} )
+            {% if is_incremental() %}
+                and modified_date::timestamp > (select max(modified_date) from {{ this }} )
+            {%- endif -%}
+        {% if not loop.last %}
+                union all
         {% endif %}
-    {% if not loop.last -%}
-        union all
-    {%- endif -%}
 {%- endfor -%}
+       ),
+       salesorderheaders AS (
+         SELECT *
+         FROM {{ source('stg', 'salesorderheader') }}
+       ),
+       final_draft as (
+       select a.sales_order_id::int
+             ,a.sales_order_detail_id::int
+            ,carrier_tracking_number::varchar(50)
+            ,order_qty::int
+            ,product_id::int
+            ,special_offer_id::int
+            ,unit_price::numeric(38, 9)
+            ,unit_price_discount::numeric(38, 9)
+            ,line_total::numeric(38, 9)
+            ,a.modified_date::timestamp
+            ,b.sub_total::numeric(38, 9) as Gross_sales
+            ,b.Total_due::numeric(38, 9) as Net_Sales
+        from cte_sales_order a
+        inner join salesorderheaders b on a.sales_order_id = b.sales_order_id
+        )
+        select *
+        from final_draft
